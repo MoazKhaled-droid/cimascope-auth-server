b4ad5319d6b4245f9817380a759b225705a0016b
moazekhaleddev-pixel <moaze.khaled.dev@gmail.com>
2026-09-23 04:30:18 +0300

    change cors sittings

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://cimascope.vercel.app",
+      "https://cimascope-xi.vercel.app",
       "https://brain-wave-quiz-app.vercel.app",
       "http://localhost:5173"
     ],
